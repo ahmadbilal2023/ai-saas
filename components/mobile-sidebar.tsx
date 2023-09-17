cc1a2a13be743930e35adcadcce16b402e7ee893
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2023-09-17 16:22:54 +0500

    update

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,16 +1,36 @@
 "use client"
 
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import { Menu } from 'lucide-react'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import SideBar from './sideBar'
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMouted] = useState(false);
+
+  useEffect(()=>{
+    setIsMouted(true);
+  },[])
+
+  if (!isMounted){
+    return null;
+  }
+
+
   return (
     <div>
+      <Sheet>
+        <SheetTrigger>
         <Button variant="ghost" size="icon" className='md:hidden'>
             <Menu />
         </Button>
+        </SheetTrigger>
+        <SheetContent side={"left"} className='p-0'>
+        <SideBar/>
+        </SheetContent>
+        </Sheet>
     </div>
   )
 }
--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -6,6 +6,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { LayoutDashboard,MessageSquare,ImageIcon,VideoIcon,Music,Code,Settings } from "lucide-react";
+import {usePathname} from "next/navigation"
+
+
+
 
 const montserrat = Montserrat({
   weight: "600",
@@ -59,6 +63,7 @@ const routes = [
 ];
 
 const SideBar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -75,7 +80,9 @@ const SideBar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+              pathname === route.href? "text-white bg-white/10" : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
